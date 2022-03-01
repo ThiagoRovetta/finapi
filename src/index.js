f029b0feca00d7138ebb265195753c889fc7a4cc
ThiagoRovetta <rovetta.thiago@gmail.com>
2022-03-01 14:50:10 -0300

    Finished app requirements.

--- src/index.js
+++ src/index.js
@@ -124,6 +124,22 @@ app.get("/account", verifyIfCPFAccountExists, (request, response) => {
   const { customer } = request;
 
   return response.status(200).json(customer);
-})
+});
+
+app.delete("/account", verifyIfCPFAccountExists, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfCPFAccountExists, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+});
 
 app.listen(3333);
